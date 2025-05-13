f4e7e632900aa8c914c334d99e29b205758172b1
Bekzod Akbarov <stx.webdev@gmail.com>
2025-05-13 15:49:46 +0500

    create an eval

---
+++ evals/reddit.eval.ts
@@ -0,0 +1,36 @@
+import { runLLM } from '../src/llm'
+import { redditToolDefinition } from '../src/tools/reddit'
+import { runEval } from './evalTools'
+import { ToolCallMatch } from './scorers'
+
+const createToolCallMessage = (toolName: string) => ({
+  role: 'assistant',
+  tool_calls: [
+    {
+      type: 'function',
+      function: { name: toolName },
+    },
+  ],
+})
+
+runEval('reddit', {
+  task: (input) =>
+    runLLM({
+      messages: [
+        {
+          role: 'user',
+          content: input,
+        },
+      ],
+      tools: [redditToolDefinition],
+      systemPrompt:
+        'You are a helpful assistant that can use the reddit tool to get the latest posts from Reddit.',
+    }),
+  data: [
+    {
+      input: 'Find me something interesting on reddit',
+      expected: createToolCallMessage(redditToolDefinition.name),
+    },
+  ],
+  scorers: [ToolCallMatch],
+})
